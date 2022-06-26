589598917847f85b32024ba98b731f4bad47f0e7
Joshua Bolchover <Josh@Joshuas-MacBook-Pro.local>
2022-06-26 18:12:33 +0300

    25/06/22

---
+++ Week-2/Day-1/Daily-Challenge/Exercise-1/script.js
@@ -0,0 +1,26 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1. remove Banana from this array.
+
+fruits.splice(0, 1);
+
+// 2. Sort the array in alphabetical order.
+
+fruits.sort();
+
+// 3. Add “Kiwi” to the end of the array.
+
+fruits.push("Kiwi");
+
+// 4. Remove “Apples” from the array. Don’t use the same method as in part 1.
+
+fruits.shift();
+
+// Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+
+fruits.reverse();
+
+console.log(fruits);
+
+
+
