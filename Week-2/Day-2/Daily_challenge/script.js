01266169692967e35fb5286abe786e0192c64855
Joshua Bolchover <Josh@Joshuas-MacBook-Pro.local>
2022-06-27 23:28:31 +0300

    26/06/22

---
+++ Week-2/Day-2/Daily_challenge/script.js
@@ -0,0 +1,14 @@
+let sentence = "The movie is bad, I like it";
+
+let wordNot = sentence.indexOf("not");
+
+let wordBad = sentence.indexOf("bad");
+
+if (wordNot < wordBad) {
+    console.log(sentence.replace(/not.*bad/, "good"));
+}
+
+else if (wordNot <! wordBad) {
+    console.log(sentence.replace(/not.*bad/, "good"));
+}
+
