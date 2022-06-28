1a6904452ded97e14b51a72d746556c6f2d32717
Joshua Bolchover <Josh@Joshuas-MacBook-Pro.local>
2022-06-28 21:24:04 +0300

    28/06/22

--- Week-2/Day-3/Daily_challenge/script.js
+++ Week-2/Day-3/Daily_challenge/script.js
@@ -1,12 +1,22 @@
+//with one loop
+let pyramid = ''
 
-let pyramid = '';
+for (i = 0; i < 6; i++) {
+    parseInt(i)
+    console.log(i * '*');
+}
+
+
+
+//two loop method
+let twoPyramid = '';
 
 for (i = 0; i < 6; i++) {
     // this means that firs ttime around, j will print only one star as it is equal to 0, the same value as i. However, as i increase each loop, j will produce more stars.
     for (j = 0; j <= i; j++) {
-        pyramid += '*';
+        twoPyramid += '*';
     }
-    pyramid += '\n';
+    twoPyramid += '\n';
 }
 
-console.log(pyramid)
+console.log(twoPyramid)
