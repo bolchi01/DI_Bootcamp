1af1c1263eef4dd8c62296248d0c1d9baa6f6ac8
Joshua Bolchover <Josh@Joshuas-MacBook-Pro.local>
2022-06-29 09:58:00 +0300

    29/06/22

---
+++ Week-2/Day-4/Class-Exercises/Exercise-1/script.js
@@ -0,0 +1,9 @@
+function ages(myAge) {
+    mumAge = 2 * myAge;
+    console.log(mumAge);
+    dadAge = 1.2 * mumAge;
+    console.log(dadAge);
+}
+
+
+ages(20);
---
+++ Week-2/Day-4/Class-Exercises/Exercise-2/script.js
@@ -0,0 +1,7 @@
+function ages(myAge) {
+    mumAge = myAge * 2;
+    return mumAge
+}
+
+ages(15);
+console.log(mumAge);
