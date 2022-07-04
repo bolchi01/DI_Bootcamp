07cd5f1dfb4800249d05a525d5a25c0d535a2acd
Joshua Bolchover <Josh@Joshuas-MacBook-Pro.local>
2022-07-04 14:54:19 +0300

    03/07/22

---
+++ Week-3/ExerciseXP/Exercise-3/script.js
@@ -0,0 +1,7 @@
+document.getElementById('navBar').setAttribute('id', 'socialNetworkNavigation');
+
+logout = document.createElement('ls');
+
+logout.innerHTML = "Logout";
+
+document.getElementsByTagName('ul')[0].appendChild(logout);
