7bc7de8703cf90d22c2974661c12a56de3301925
Joshua Bolchover <Josh@Joshuas-MacBook-Pro.local>
2022-07-12 09:58:51 +0300

    changes

--- Week-4/Day-2/Daily-challenge/script.js
+++ Week-4/Day-2/Daily-challenge/script.js
@@ -5,7 +5,7 @@ let groceries = {
     vegetables: ["tomatoes", "cucumber", "salad"],
     totalPrice : "20$", // we will see this change as it is in an object, a dynamic data type
     other : {
-        payed : false, // we will see this change as it is in an object, a dynamic data type
+        payed : true, // we will see this change as it is in an object, a dynamic data type
         meansOfPayment : ["cash", "creditCard"]
     }
 }
@@ -23,8 +23,10 @@ const cloneGroceries = () => {
     console.log("user", user, "client", client); // we will not see this change to Betty as this is a primitive data type
     let shopping = groceries;
     groceries.totalPrice = "35$";
-    console.log("shoppingPrice", shopping.totalPrice, "groceriesPrice", groceries.totalPrice); // you will see this change as it is a dynamic data type
+    console.log("shoppingPrice", shopping.totalPrice, "groceriesPrice", groceries.totalPrice);
+    groceries.other.payed = "false"
+    console.log("shoppingPayed", shopping.other.payed, "groceriesPayed", groceries.other.payed); // you will see this change as it is a dynamic data type
 }
 
-cloneGroceries(); // does this do much - the task did not tell me to do anything else
+cloneGroceries(); 
 
