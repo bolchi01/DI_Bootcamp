879d63de5c76595741da37c8b2066ab39f2ac561
Joshua Bolchover <Josh@Joshuas-MacBook-Pro.local>
2022-07-31 13:03:50 +0300

    changes

---
+++ Week-7/Day-1/Exercise-XP/npm-mode/main.js
@@ -0,0 +1,14 @@
+// let a = 5;
+// let b = 10;
+
+// console.log(`Hello you are ${a + b} years old.`);
+
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+
+
+console.log(`First run: ${a + b}`);
+console.log(`second run: ${x + c}`);
